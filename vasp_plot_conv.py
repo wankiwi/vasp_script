61ebea5a4d0375432646328296c8b197700df4ba
kiwi <46419582+wankiwi@users.noreply.github.com>
2021-01-03 14:41:21 +0800

    vasp_plot_conv.py bug fix

--- vasp_plot_conv.py
+++ vasp_plot_conv.py
@@ -3,7 +3,7 @@
 
 __author__ = 'wankw (wankaiweii@gmail.com)' 
 
-import xml.etree.cElementTree as et
+import re
 import numpy as np
 from plotille import *
 from matplotlib import pyplot as plt
@@ -42,7 +42,7 @@ def mp_plot(x_data,y_data,x_label,y_label):
 
 
 def get_version():
-    return '1.3 (2020.12.30, wankaiweii@gmail.com)'
+    return '1.4 (2021.1.3, wankaiweii@gmail.com)'
 
 parser = argparse.ArgumentParser(description='Plot the convergence curve in VASP calculation.')
 
@@ -62,69 +62,78 @@ plot_method = args.plot_method
 last_n = args.last_n
 log_mode = args.log_mode
 
-#parse vasprun.xml
-tree = et.parse('vasprun.xml')
-root = tree.getroot()
 
-#incar
-EDIFFG = [i.text for i in root[[i.tag for i in root].index('incar')] 
-            if i.attrib['name'] == 'EDIFFG'][0]       
-EDIFFG = float(EDIFFG)
+#atom information list & selective dynamics list 
+with open('POSCAR') as file:
+    poscar = file.readlines()
 
-#atom information
-atominfo_list = [j[0].text for j in [i for i in root.findall('atominfo/array/set')][0]]
+a_vector = np.array(poscar[2].split(),dtype=float)
+b_vector = np.array(poscar[3].split(),dtype=float)
+c_vector = np.array(poscar[4].split(),dtype=float)
 
-#selective dynamics list
+tmp_list1 = poscar[5].split()
+tmp_list2 = list(map(int, poscar[6].split()))
+atominfo_list = ''
+for i in range(len(tmp_list1)):
+    atominfo_list+=(f'{tmp_list1[i]} '*tmp_list2[i])
+atominfo_list = atominfo_list.split()
+
 selective_list = []
-selective_tag_array = []
-try:
-    for j in [i for i in root.findall('structure/varray') if i.attrib['name'] == 'selective'][0]:
-        selective_list.append(j.text)
-        selective_tag_array.append(j.text.replace('T','1').replace('F','0').split())
-    selective_tag_array = np.array(selective_tag_array,dtype=int)
-except:
-    [selective_list.append('  T  T  T ') for i in range(len(atominfo_list))] 
-    selective_tag_array = np.ones((len(atominfo_list),3))
+selec_tag = re.compile(r'[TF]')
+if 's' in poscar[7] or 'S' in poscar[7]:
+    for i in poscar[9:9+len(atominfo_list)]:
+        selective_list.append(selec_tag.findall(i))
+else:
+    for i in range(sum(tmp_list2)):
+        selective_list.append(['T', 'T', 'T'])
+selective_list_array = np.array(selective_list)
+selective_list_array[selective_list_array=='T'] = 1
+selective_list_array[selective_list_array=='F'] = 0
+selective_list_array = np.array(selective_list_array, dtype=int)
+
+
+with open ('OUTCAR') as file:
+    outcar = file.read()
+    
+#EDIFFG
+EDIFFG=re.findall('EDIFFG = (.*)   stopping-criterion for IOM', outcar)[0]
+EDIFFG=float(EDIFFG)
 
 #The number of atoms fixed in x y z direction
-n_fix = len([i for i in selective_tag_array if all(i == np.array([0,0,0]))])
-n = np.shape(selective_tag_array)[0] - n_fix
+n_fix = len([i for i in selective_list_array if all(i == np.array([0,0,0]))])
+n = np.shape(selective_list_array)[0] - n_fix
 
-#force list
-force_list = []
-selective_force_list = []
+#force & position list
+force_tag = re.compile(f'TOTAL-FORCE \(eV/Angst\)\n {"-"*83}\n(.*?)\n -', re.DOTALL)
+force_array = [i.split() for i in force_tag.findall(outcar)]
+force_array = np.array(force_array,dtype=float)
+dim1,dim2 = force_array.shape
+force_array = force_array[:, :, np.newaxis]
+force_array = force_array.reshape((dim1, int(dim2/6), 6))
+
+total_force_list = []
 max_force_list = []
-max_force_atom_list = []
+max_force_atom_seq_list = []
 ave_force_list = []
-for j in [i for i in root.findall('calculation/varray') if i.attrib['name'] == 'forces']:    
-    force_array = [k.text.split() for k in j]
-    force_array = np.array(force_array,dtype=float)
-    selective_force_array = selective_tag_array * force_array      
-    selective_force = [(i[0]**2 + i[1]**2 + i[2]**2)**0.5  for i in selective_force_array]
-
-    max_f = max(selective_force)
+for i in force_array:
+    total_f_ionic_step = [(j[0]**2 + j[1]**2 + j[2]**2)**0.5 for j in (i[:,3:6] *  selective_list_array)]
+    total_force_list.append(total_f_ionic_step)
+    
+    max_f = max(total_f_ionic_step)
     max_force_list.append(max_f)
     
-    max_f_atom = selective_force.index(max_f) + 1
-    max_force_atom_list.append(max_f_atom)
+    max_force_atom_seq = total_f_ionic_step.index(max_f) + 1
+    max_force_atom_seq_list.append(max_force_atom_seq)
     
-    ave_f = sum(selective_force)/n
+    ave_f = sum(total_f_ionic_step)/n
     ave_force_list.append(ave_f)
     
-    force_list.append(force_array)
-    selective_force_list.append(selective_force)
     
-force_array = np.array(force_list)
-selective_force_array = np.array(selective_force_list)
-selective_force_array = selective_force_array[:, :, np.newaxis]
-
-force_log_array = np.c_[force_array, selective_force_array]
+total_force_array = np.array(total_force_list, dtype=float)
 
 #energy list
-energy_list = []
-for j in [i for i in root.findall('calculation')]:
-    energy_list.append(float([k.text for k in j.findall('scstep/energy/i')][-1]))
-
+energy_tag = re.compile(f'total drift.*?energy\(sigma->0\) =(.*?)\n', re.DOTALL)
+energy_array = np.array(energy_tag.findall(outcar), dtype=float)
 
 if y_variable == 'f':
     y_data = max_force_list[-last_n:]
@@ -132,8 +141,8 @@ if y_variable == 'f':
     x_label = 'Step'
     y_label = 'max_F (eV/A)'
 elif y_variable == 'e':
-    y_data = energy_list[-last_n:]
-    x_data = list(range(1,len(energy_list)+1))[-last_n:]
+    y_data = energy_array[-last_n:]
+    x_data = list(range(1,len(energy_array)+1))[-last_n:]
     x_label = 'Step'
     y_label = 'Energy (eV)'
    
@@ -149,30 +158,21 @@ if max_force_list[-1] >= abs(EDIFFG):
     print(f'[set EDIFFG:{EDIFFG:.2e} (eV/A), not converged!]')
 else:
     print(f'[set EDIFFG:{EDIFFG:.2e} (eV/A), converged!]')    
-print(f'After {len(max_force_list)} ionic steps, max force converged to  {max_force_list[-1]:.6f} at atom {max_force_atom_list[-1]}, energy(sigma->0): {energy_list[-1]}.')
-print(f'{n_fix} of {np.shape(selective_tag_array)[0]} atoms were fixed, average force: {ave_force_list[-1]:.6f}.')
+print(f'After {len(max_force_list)} ionic steps, max force converged to  {max_force_list[-1]:.6f} at atom {max_force_atom_seq_list[-1]}, energy(sigma->0): {energy_array[-1]}.')
+print(f'{n_fix} of {len(atominfo_list)} atoms were fixed, average force: {ave_force_list[-1]:.6f}.')
 
 
-if log_mode:
-    #force&energy log for every ionic step
-    pos_list = []
-    for j in [i for i in root.findall('calculation/structure/varray')]:
-        pos_list.append([k.text.split() for k in j])
-        
-    pos_array = np.array(pos_list, dtype=float)
-    
-    (step_num, atom_num, temp) = np.shape(pos_array)
-    with open('check_conv.log','w+') as log:
-        for i in range(step_num):
-            log.write(f'Ionic step: {i+1:>4}\n')
-            log.write('-------atom-------||---------Position x y z--------------||-------------------Force x y z total-----------\n')
-            for j in range(atom_num):
-                log.write(f'{j+1:>4}{atominfo_list[j]:>4}{selective_list[j]}  ')
-                [log.write(f'{k: 12.7f}') for k in pos_array[i][j]]
-                log.write('  ')
-                [log.write(f'{k: 12.7f}') for k in force_log_array[i][j]]
-                log.write('\n')
-            log.write('----------------------------------------------------------------------------------------------------------\n')
-            log.write(f'After {len(max_force_list)} ionic steps, max force converged to  {max_force_list[-1]:.6f} at atom {max_force_atom_list[-1]}, energy(sigma->0): {energy_list[-1]}.\n')
-            log.write(f'{n_fix} of {np.shape(selective_tag_array)[0]} atoms were fixed, average force: {ave_force_list[-1]:.6f}.\n\n')
-    print ('\ncheck_conv.log file generated successfully!')
+(step_num, atom_num) = np.shape(total_force_array)
+with open('check_conv.log','w+') as log:
+    for i in range(step_num):
+        log.write(f'Ionic step: {i+1:>4}\n')
+        log.write('-------atom-------||---------Position x y z----------------||-------------------Force x y z total------------\n')
+        for j in range(atom_num):
+            log.write(f'{j+1:>4}{atominfo_list[j]:>4}  ')
+            [log.write(f'{k:3}') for k in selective_list[j]]
+            [log.write(f'{k: 13.7f}') for k in force_array[i][j]]
+            log.write(f'{total_force_array[i][j]: 12.7f}')
+            log.write('\n')
+        log.write('-------------------------------------------------------------------------------------------------------------\n')
+        log.write(f'After {len(max_force_list)} ionic steps, max force converged to  {max_force_list[i]:.6f} at atom {max_force_atom_seq_list[i]}, energy(sigma->0): {energy_array[i]}.\n')
+        log.write(f'{n_fix} of {len(atominfo_list)} atoms were fixed, average force: {ave_force_list[i]:.6f}.\n\n')
